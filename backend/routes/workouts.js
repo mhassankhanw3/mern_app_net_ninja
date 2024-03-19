eae204f8306521309fc6cc1ddb591a0ba935296e
mhassankhanw3 <mhassankhanw3@gmail.com>
2024-03-20 00:20:23 +0500

    middlware added

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -7,8 +7,11 @@ const {
   deleteWorkout,
   updateWorkout,
 } = require("../controller/workout.controller.js");
+const requireAuth = require("../middleware/requireAuth.js");
 
 const router = express.Router();
+
+router.use(requireAuth);
 
 router.get("/", getWorkouts);
 
